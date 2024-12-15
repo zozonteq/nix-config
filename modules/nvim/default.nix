49623661948fd28edf2e5a963ca4d48832883f9b
zozonteq <ottotak@outlook.com>
2024-12-15 22:21:10 +0900

    :sparkles: Update neovim config
    change colorscheme to iceberg!

--- modules/nvim/default.nix
+++ modules/nvim/default.nix
@@ -1,27 +1,63 @@
-{
-  lib,
-  config,
-  pkgs,
-  ...
-}: {
+{pkgs, ...}: {
   programs.nvf = {
     enable = true;
     settings = {
+      # todo: vimの入れ子にしたかった...
       vim.viAlias = true;
       vim.vimAlias = true;
       vim.lsp = {
         enable = true;
+        formatOnSave = true;
+        null-ls.enable = true;
+        lspkind.enable = true;
+      };
+      vim.autocomplete.nvim-cmp = {
+        enable = true;
+      };
+      vim.ui = {
+        noice.enable = true;
+        colorizer.enable = true;
+        breadcrumbs = {
+          enable = true;
+          lualine.winbar.enable = true;
+          lualine.winbar.alwaysRender = true;
+        };
+      };
+      vim.statusline.lualine = {
+        enable = true;
+        theme = "iceberg";
+      };
+      vim.tabline.nvimBufferline.enable = true;
+      vim.languages = {
+        enableFormat = true; # enable formatting for languages
+        rust.enable = true;
+        nix = {
+          enable = true;
+          lsp.enable = true;
+          format.enable = true;
+          treesitter.enable = true;
+        };
+        ts.enable = true;
+        clang.enable = true;
+        python.enable = true;
+        html.enable = true;
+        lua.enable = true;
       };
       vim.useSystemClipboard = true;
-      vim.startPlugins = with pkgs.vimPlugins;[ 
+      vim.telescope.enable = true;
+      vim.startPlugins = with pkgs.vimPlugins; [
         nvim-treesitter
-        telescope-nvim
-        catppuccin-nvim
         vim-wakatime
+        nvim-autopairs
       ];
-      vim.lazy.plugins = {
+      vim.lazy.plugins = with pkgs.vimPlugins; {
+        "iceberg.vim" = {
+          package = iceberg-vim;
+          lazy = true;
+          event = ["VimEnter"];
+        };
         "aerial.nvim" = {
-          package = pkgs.vimPlugins.aerial-nvim;
+          package = aerial-nvim;
           setupModule = "aerial";
           setupOpts = {
             option_name = true;
--- modules/wezterm/wezterm.lua
+++ modules/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require "wezterm"
 
 local config = wezterm.config_builder()
-config.color_scheme = 'GruvboxDarkHard'
+config.color_scheme ="iceberg-dark" 
 config.font = wezterm.font "HackGen Console NF"
 
 config.window_background_opacity =0.9
