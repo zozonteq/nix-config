dd4e3b38615a2a6e8ff8676bc6461fd3f9a2ae8b
oto <ottotak@outlook.com>
2024-12-18 16:28:54 +0900

    ✨ Add host

--- flake.nix
+++ flake.nix
@@ -15,47 +15,49 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = {
-    nixpkgs,
-    home-manager,
-    nix-darwin,
-    nvf,
-    nixvim,
-    ...
-  }: {
-    darwinConfigurations = {
-      mbp2016 = nix-darwin.lib.darwinSystem {
-        system = "x86_64-darwin";
-        modules = [./conf/mbp2016-darwin.nix];
-      };
-      mba2023 = nix-darwin.lib.darwinSystem {
-        system = "aarch64-darwin";
-        module = [./conf/mbp2016-darwin.nix];
-      };
-    };
-    homeConfigurations = {
-      mba2023 = home-manager.lib.homeManagerConfiguration {
-        pkgs = import nixpkgs {
-          system = "aarch64-darwin";
-          config.allowUnFree = true;
-        };
-        modules = [
-          nixvim.homeManagerModules.default
-          nvf.homeManagerModules.default
-          ./conf/mbp2016-home.nix
-        ];
-      };
-      mbp2016 = home-manager.lib.homeManagerConfiguration {
-        pkgs = import nixpkgs {
+  outputs =
+    {
+      nixpkgs,
+      home-manager,
+      nix-darwin,
+      nvf,
+      nixvim,
+      ...
+    }:
+    {
+      darwinConfigurations = {
+        mbp2016 = nix-darwin.lib.darwinSystem {
           system = "x86_64-darwin";
-          config.allowUnFree = true;
+          modules = [ ./conf/mbp2016-darwin.nix ];
         };
-        modules = [
-          nixvim.homeManagerModules.default
-          nvf.homeManagerModules.default
-          ./conf/mbp2016-home.nix
-        ];
+        mba2023 = nix-darwin.lib.darwinSystem {
+          system = "aarch64-darwin";
+          modules = [ ./conf/mbp2016-darwin.nix ];
+        };
+      };
+      homeConfigurations = {
+        mba2023 = home-manager.lib.homeManagerConfiguration {
+          pkgs = import nixpkgs {
+            system = "aarch64-darwin";
+            config.allowUnFree = true;
+          };
+          modules = [
+            nixvim.homeManagerModules.default
+            nvf.homeManagerModules.default
+            ./conf/mbp2016-home.nix
+          ];
+        };
+        mbp2016 = home-manager.lib.homeManagerConfiguration {
+          pkgs = import nixpkgs {
+            system = "x86_64-darwin";
+            config.allowUnFree = true;
+          };
+          modules = [
+            nixvim.homeManagerModules.default
+            nvf.homeManagerModules.default
+            ./conf/mbp2016-home.nix
+          ];
+        };
       };
     };
-  };
 }
