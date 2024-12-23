14be52431b164c1a472d2cde747263a21afb71c7
oto <ottotak@outlook.com>
2024-12-23 22:29:19 +0900

    🔧 Optimize font size

--- modules/wezterm/wezterm.lua
+++ modules/wezterm/wezterm.lua
@@ -1,5 +1,13 @@
 local wezterm = require("wezterm")
 
+function GetHostname()
+	local f = io.popen("/bin/hostname")
+	local hostname = f:read("*a") or ""
+	f:close()
+	hostname = string.gsub(hostname, "\n$", "")
+	return hostname
+end
+
 local config = wezterm.config_builder()
 
 config.color_scheme = "cyberdream"
@@ -10,6 +18,11 @@ config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
 config.show_new_tab_button_in_tab_bar = false
 config.macos_window_background_blur = 30
+
+print("Hello!")
+if GetHostname() == "mac2" then
+	config.font_size = 18.0
+end
 
 -- https://github.com/NixOS/nixpkgs/issues/336069
 config.front_end = "WebGpu"
