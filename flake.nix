58595e740511943a50bba3b50d8185f2abbc6e07
oto <ottotak@outlook.com>
2025-02-03 16:40:51 +0900

    ✨ Alt-Tab, update packages

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixvim = {
-      url = "github:nix-community/nixvim/update/main";
+      url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-on-droid = {
--- hosts/darwin/shared.nix
+++ hosts/darwin/shared.nix
@@ -96,6 +96,7 @@
       "ghostty"
       "barrier"
       "keycastr"
+      "alt-tab"
     ];
   };
 
